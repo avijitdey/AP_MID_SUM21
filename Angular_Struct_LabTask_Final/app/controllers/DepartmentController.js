a53cc50d9216a380f323bf91ff66b1d2abc94949
avijitdey <44554969+avijitdey@users.noreply.github.com>
2021-08-21 13:46:04 +0300

    Delete Angular_Struct_LabTask_Final directory

--- Angular_Struct_LabTask_Final/app/controllers/DepartmentController.js
+++
@@ -1,7 +0,0 @@
-app.controller("DepartmentController", function ($scope, $http, ajax) {
-	ajax.get("https://localhost:44353/api/Department/GetAll", success, error);
-	function success(response) {
-		$scope.departments = response.data;
-	}
-	function error(error) {}
-});
